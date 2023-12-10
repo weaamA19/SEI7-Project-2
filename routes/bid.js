cca02da137c65d5584a102936ff27efb5dd79bab
Mazen Al-Maskati <m@mmt.me>
2023-12-10 18:16:35 +0300

    Bids Model & Add & Delete

--- routes/bid.js
+++ routes/bid.js
@@ -11,14 +11,14 @@ router.get('/index', bidCntrl.bid_index_get);
 //router.get("/detail", bidCntrl.bid_show_get);
 
 //adding a bid (add and update)
-//router.get('/add', bidCntrl.bid_create_get);
-//router.post('/add', bidCntrl.bid_create_post);
+router.get('/add', bidCntrl.bid_add_get);
+router.post('/create', bidCntrl.bid_create_post);
 
 //editing a bid (edit and update)
 //router.get('/edit', bidCntrl.bid_edit_get);
 //router.post('/update', bidCntrl.bid_update_post);
 
 //deleting a bid (might change to cancel or delete)
-//router.post('/delete', bidCntrl.bid_delete_post);
+router.post('/delete', bidCntrl.bid_delete_post);
 
 module.exports = router;
