27dd1e25ca955bcdd73015c91dd2cc53800c4971
weaamA19 <weaam.ajoor@gmail.com>
2023-12-13 18:29:48 +0300

    adding user typre restrictions on the routes

--- routes/bid.js
+++ routes/bid.js
@@ -5,6 +5,8 @@ const router = express.Router();
 // Check if the user is logged in - Weaam
 const ensureLoggedIn = require("../config/ensureLoggedIn");
 
+const { checkType } = require("../config/checkType");
+
 const currency = require('../config/settings'); //if we want to use currency add it to req.locals
 
 //so the controller can encode the data from the form
@@ -12,22 +14,22 @@ router.use(express.urlencoded({ extended: true }));
 
 const bidCntrl = require("../controllers/bid");
 
-router.get("/index", ensureLoggedIn, bidCntrl.bid_index_get);
-router.get("/detail", bidCntrl.bid_show_get);
+router.get("/index", ensureLoggedIn, checkType(1), bidCntrl.bid_index_get);
+router.get("/detail", checkType(1), bidCntrl.bid_show_get);
 
 //adding a bid (add and update)
-router.get("/add", ensureLoggedIn, bidCntrl.bid_add_get);
-router.post("/create", ensureLoggedIn, bidCntrl.bid_create_post);
+router.get("/add", ensureLoggedIn, checkType(1), bidCntrl.bid_add_get);
+router.post("/create", ensureLoggedIn, checkType(1), bidCntrl.bid_create_post);
 
-router.get("/new", ensureLoggedIn, bidCntrl.bidder_add_get);
+router.get("/new", ensureLoggedIn, checkType(1), bidCntrl.bidder_add_get);
 
 //editing a bid (edit and update)
-router.get("/edit", ensureLoggedIn, bidCntrl.bid_edit_get);
-router.post("/update", ensureLoggedIn, bidCntrl.bid_update_post);
+router.get("/edit", ensureLoggedIn, checkType(1), bidCntrl.bid_edit_get);
+router.post("/update", ensureLoggedIn, checkType(1), bidCntrl.bid_update_post);
 
 //deleting a bid (might change to cancel or delete)
-router.post("/delete", ensureLoggedIn, bidCntrl.bid_delete_post);
+router.post("/delete", ensureLoggedIn, checkType(1), bidCntrl.bid_delete_post);
 
-router.get("/userBids", ensureLoggedIn, bidCntrl.user_bid_get);
+router.get("/userBids", ensureLoggedIn, checkType(2), checkType(3), bidCntrl.user_bid_get);
 
 module.exports = router;
--- routes/category.js
+++ routes/category.js
@@ -2,18 +2,19 @@ const express = require("express");
 const router = express.Router();
 router.use(express.urlencoded({ extended: true }));
 const categoryCntrl = require("../controllers/category");
+const {checkType} = require("../config/checkType");
 
 // Check if the user is logged in - Weaam
 const ensureLoggedIn = require("../config/ensureLoggedIn");
 
 // Routes
-router.get("/add", ensureLoggedIn, categoryCntrl.category_create_get);
-router.post("/add", ensureLoggedIn, categoryCntrl.category_create_post);
-router.get("/index", ensureLoggedIn, categoryCntrl.category_index_get);
-router.get("/detail", ensureLoggedIn, categoryCntrl.category_show_get);
+router.get("/add", ensureLoggedIn, checkType(1), categoryCntrl.category_create_get);
+router.post("/add", ensureLoggedIn, checkType(1), categoryCntrl.category_create_post);
+router.get("/index", ensureLoggedIn, checkType(1), categoryCntrl.category_index_get);
+router.get("/detail", ensureLoggedIn, checkType(1), categoryCntrl.category_show_get);
 
-router.get("/edit", ensureLoggedIn, categoryCntrl.category_edit_get);
-router.post("/update", ensureLoggedIn, categoryCntrl.category_update_post);
+router.get("/edit", ensureLoggedIn, checkType(1), categoryCntrl.category_edit_get);
+router.post("/update", ensureLoggedIn, checkType(1), categoryCntrl.category_update_post);
 
-router.post("/delete", ensureLoggedIn, categoryCntrl.category_delete_get);
+router.post("/delete", ensureLoggedIn, checkType(1), categoryCntrl.category_delete_get);
 module.exports = router;
