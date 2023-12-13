74ae813df911d66f8a6f171c6cccf059f02dc0c2
Mazen Al-Maskati <m@mmt.me>
2023-12-13 19:02:23 +0300

    fixed privs by checktype

--- routes/bid.js
+++ routes/bid.js
@@ -21,7 +21,7 @@ router.get("/detail", checkType(1), bidCntrl.bid_show_get);
 router.get("/add", ensureLoggedIn, checkType(1), bidCntrl.bid_add_get);
 router.post("/create", ensureLoggedIn, checkType(1), bidCntrl.bid_create_post);
 
-router.get("/new", ensureLoggedIn, checkType(1), bidCntrl.bidder_add_get);
+router.get("/new", ensureLoggedIn, checkType(3), bidCntrl.bidder_add_get);
 
 //editing a bid (edit and update)
 router.get("/edit", ensureLoggedIn, checkType(1), bidCntrl.bid_edit_get);
